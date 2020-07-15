88c8e41f4d64f732960c5e6a8431a11601d305fc
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-07-15 19:33:36 +0530

    Added : Welcome Message

---
+++ empWageComputation.java
@@ -0,0 +1,8 @@
+//This is EmpPresentAbsent //
+
+public class empWageComputation{
+
+	public static void main(String args[]){
+			System.out.println("Welcome to the Employee Wage Computation Problem");
+	}
+}
